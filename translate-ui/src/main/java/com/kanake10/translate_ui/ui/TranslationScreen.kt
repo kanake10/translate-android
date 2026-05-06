97d22bc854030998b3ffa35536bb99bc0a9407bf
kanake10 <10kanake@gmail.com>
2026-05-06 19:51:30 +0300

    ensure dropdown menu launches

--- translate-ui/src/main/java/com/kanake10/translate_ui/ui/TranslationScreen.kt
+++ translate-ui/src/main/java/com/kanake10/translate_ui/ui/TranslationScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -209,6 +210,7 @@ internal fun TranslateLanguageSelector(
                 },
                 modifier = Modifier
                     .fillMaxWidth()
+                    .menuAnchor(MenuAnchorType.PrimaryNotEditable, true)
             )
 
             ExposedDropdownMenu(
@@ -245,6 +247,7 @@ internal fun TranslateLanguageSelector(
                 },
                 modifier = Modifier
                     .fillMaxWidth()
+                    .menuAnchor(MenuAnchorType.PrimaryNotEditable, true)
             )
 
             ExposedDropdownMenu(
